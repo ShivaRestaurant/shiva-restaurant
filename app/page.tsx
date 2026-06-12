92722a275fa0611b8f74a83ee427dc4f1ebc727a
ShivaRestaurant <khanna.j@hotmail.de>
2026-06-12 21:22:27 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -216,7 +216,7 @@ export default function Home() {
         <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-16">
 
           <div>
-            <section id="kontakt" className="py-24 px-6 bg-black"></section>
+            <section id="kontakt" className="py-24 px-6 bg-black">
               Kontakt
             </h2>
 
